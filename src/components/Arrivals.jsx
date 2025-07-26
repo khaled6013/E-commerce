67b915abec0b8b0f47a86ec94f502ecc9f8c8340
khaled6013 <kmd601384@gmail.com>
2025-07-26 18:06:54 +0600

    arrivals top final

--- src/components/Arrivals.jsx
+++ src/components/Arrivals.jsx
@@ -2,9 +2,14 @@ import { FaHeart, FaLongArrowAltLeft, FaLongArrowAltRight, FaShoppingCart } from
 import Slider from "react-slick";
 import ariImg from '../assets/ari1.png';
 import { FaCodeCompare } from "react-icons/fa6";
-// import ari2Img from '../assets/ar2.png'
-// import ari3Img from '../assets/ar3.png'
-// import ari4Img from '../assets/ar4.png'
+import ari2Img from '../assets/ari2.png'
+import ari3Img from '../assets/ari3.png'
+import ari4Img from '../assets/ari4.png'
+import ari5Img from '../assets/ari5.png'
+import ari6Img from '../assets/ari6.png'
+import ari7Img from '../assets/ari7.png'
+import ari8Img from '../assets/ari8.png'
+import addd2 from'../assets/addd2.png'
 
 const Arrivals = () => {
    const settings = {
@@ -49,7 +54,7 @@ const Arrivals = () => {
          <div className="w-full lg:mt-[40px]">
             <Slider {...settings} className="gap-3" >
 
-                 
+
                   <div className=" !w-[290px] relative group">
                         <div className="w-full ">
                             <img src={ariImg} alt="" className="w-full" />
@@ -61,8 +66,8 @@ const Arrivals = () => {
                         <div className="absolute top-2 left-2">
                           <p className="text-[#FFFFFF]  text-[14px] font-dm font-bold py-[7px] px-[28px] bg-black hover:bg-[#2e2b2b] cursor-pointer">New</p>
                         </div>
-                        <div className="absolute left-0 bottom-[50px]">
-                          <div className="w-[270px] h-[0px] group-hover:h-[130px] duration-300 ease-in-out bg-[#FFFFFF]">
+                        <div className="absolute left-0 bottom-[42px]">
+                          <div className="w-[290px] h-[0px] group-hover:h-[130px] duration-300 ease-in-out bg-[#FFFFFF]">
                             <div className="pt-[26px] opacity-0 group-hover:opacity-95">
                               <div className="flex items-center justify-end mr-3">
                                  <p className="text-[#262626] hover:text-[#767676] text-[15px] font-dm font-bold hover:font-normal pr-2 cursor-pointer">Add to Wish List</p>
@@ -81,7 +86,7 @@ const Arrivals = () => {
                   </div>
                   <div className=" !w-[290px] relative group">
                         <div className="w-full ">
-                            <img src={ariImg} alt="" className="w-full" />
+                            <img src={ari2Img} alt="" className="w-full" />
                         </div>
                         <div className="flex justify-between mt-5 z-10">
                             <p className="text-[#262626] text-[16px] font-dm font-bold">Basic Crew  Neck Tee</p>
@@ -90,8 +95,8 @@ const Arrivals = () => {
                         <div className="absolute top-2 left-2">
                           <p className="text-[#FFFFFF]  text-[14px] font-dm font-bold py-[7px] px-[28px] bg-black hover:bg-[#2e2b2b] cursor-pointer">New</p>
                         </div>
-                        <div className="absolute left-0 bottom-[50px]">
-                          <div className="w-[270px] h-[0px] group-hover:h-[130px] duration-300 ease-in-out bg-[#FFFFFF]">
+                        <div className="absolute left-0 bottom-[42px]">
+                          <div className="w-[290px] h-[0px] group-hover:h-[130px] duration-300 ease-in-out bg-[#FFFFFF]">
                             <div className="pt-[26px] opacity-0 group-hover:opacity-95">
                               <div className="flex items-center justify-end mr-3">
                                  <p className="text-[#262626] hover:text-[#767676] text-[15px] font-dm font-bold hover:font-normal pr-2 cursor-pointer">Add to Wish List</p>
@@ -110,7 +115,7 @@ const Arrivals = () => {
                   </div>
                   <div className=" !w-[290px] relative group">
                         <div className="w-full ">
-                            <img src={ariImg} alt="" className="w-full" />
+                            <img src={ari3Img} alt="" className="w-full" />
                         </div>
                         <div className="flex justify-between mt-5 z-10">
                             <p className="text-[#262626] text-[16px] font-dm font-bold">Basic Crew  Neck Tee</p>
@@ -119,8 +124,8 @@ const Arrivals = () => {
                         <div className="absolute top-2 left-2">
                           <p className="text-[#FFFFFF]  text-[14px] font-dm font-bold py-[7px] px-[28px] bg-black hover:bg-[#2e2b2b] cursor-pointer">New</p>
                         </div>
-                        <div className="absolute left-0 bottom-[50px]">
-                          <div className="w-[270px] h-[0px] group-hover:h-[130px] duration-300 ease-in-out bg-[#FFFFFF]">
+                        <div className="absolute left-0 bottom-[42px]">
+                          <div className="w-[290px] h-[0px] group-hover:h-[130px] duration-300 ease-in-out bg-[#FFFFFF]">
                             <div className="pt-[26px] opacity-0 group-hover:opacity-95">
                               <div className="flex items-center justify-end mr-3">
                                  <p className="text-[#262626] hover:text-[#767676] text-[15px] font-dm font-bold hover:font-normal pr-2 cursor-pointer">Add to Wish List</p>
@@ -139,7 +144,7 @@ const Arrivals = () => {
                   </div>
                   <div className=" !w-[290px] relative group">
                         <div className="w-full ">
-                            <img src={ariImg} alt="" className="w-full" />
+                            <img src={ari4Img} alt="" className="w-full" />
                         </div>
                         <div className="flex justify-between mt-5 z-10">
                             <p className="text-[#262626] text-[16px] font-dm font-bold">Basic Crew  Neck Tee</p>
@@ -148,8 +153,8 @@ const Arrivals = () => {
                         <div className="absolute top-2 left-2">
                           <p className="text-[#FFFFFF]  text-[14px] font-dm font-bold py-[7px] px-[28px] bg-black hover:bg-[#2e2b2b] cursor-pointer">New</p>
                         </div>
-                        <div className="absolute left-0 bottom-[50px]">
-                          <div className="w-[270px] h-[0px] group-hover:h-[130px] duration-300 ease-in-out bg-[#FFFFFF]">
+                        <div className="absolute left-0 bottom-[42px]">
+                          <div className="w-[290px] h-[0px] group-hover:h-[130px] duration-300 ease-in-out bg-[#FFFFFF]">
                             <div className="pt-[26px] opacity-0 group-hover:opacity-95">
                               <div className="flex items-center justify-end mr-3">
                                  <p className="text-[#262626] hover:text-[#767676] text-[15px] font-dm font-bold hover:font-normal pr-2 cursor-pointer">Add to Wish List</p>
@@ -168,7 +173,7 @@ const Arrivals = () => {
                   </div>
                   <div className=" !w-[290px] relative group">
                         <div className="w-full ">
-                            <img src={ariImg} alt="" className="w-full" />
+                            <img src={ari5Img} alt="" className="w-full" />
                         </div>
                         <div className="flex justify-between mt-5 z-10">
                             <p className="text-[#262626] text-[16px] font-dm font-bold">Basic Crew  Neck Tee</p>
@@ -177,8 +182,8 @@ const Arrivals = () => {
                         <div className="absolute top-2 left-2">
                           <p className="text-[#FFFFFF]  text-[14px] font-dm font-bold py-[7px] px-[28px] bg-black hover:bg-[#2e2b2b] cursor-pointer">New</p>
                         </div>
-                        <div className="absolute left-0 bottom-[50px]">
-                          <div className="w-[270px] h-[0px] group-hover:h-[130px] duration-300 ease-in-out bg-[#FFFFFF]">
+                        <div className="absolute left-0 bottom-[42px]">
+                          <div className="w-[290px] h-[0px] group-hover:h-[130px] duration-300 ease-in-out bg-[#FFFFFF]">
                             <div className="pt-[26px] opacity-0 group-hover:opacity-95">
                               <div className="flex items-center justify-end mr-3">
                                  <p className="text-[#262626] hover:text-[#767676] text-[15px] font-dm font-bold hover:font-normal pr-2 cursor-pointer">Add to Wish List</p>
@@ -198,6 +203,165 @@ const Arrivals = () => {
 
             </Slider>
          </div>
+       </div>
+       <div className="lg:w-10/12 mx-auto w-11/12">
+        <div className="lg:mt-[150px]">
+           <Slider {...settings} className="gap-3" >
+
+
+                  <div className=" !w-[290px] relative group">
+                        <div className="w-full ">
+                            <img src={ari6Img} alt="" className="w-full" />
+                        </div>
+                        <div className="flex justify-between mt-5 z-10">
+                            <p className="text-[#262626] text-[16px] font-dm font-bold">Basic Crew  Neck Tee</p>
+                            <p className="text-[#767676] text-[16px] font-normal font-dm">$44.00</p>
+                        </div>
+                        <div className="absolute top-2 left-2">
+                          <p className="text-[#FFFFFF]  text-[14px] font-dm font-bold py-[7px] px-[28px] bg-black hover:bg-[#2e2b2b] cursor-pointer">New</p>
+                        </div>
+                        <div className="absolute left-0 bottom-[42px]">
+                          <div className="w-[290px] h-[0px] group-hover:h-[130px] duration-300 ease-in-out bg-[#FFFFFF]">
+                            <div className="pt-[26px] opacity-0 group-hover:opacity-95">
+                              <div className="flex items-center justify-end mr-3">
+                                 <p className="text-[#262626] hover:text-[#767676] text-[15px] font-dm font-bold hover:font-normal pr-2 cursor-pointer">Add to Wish List</p>
+                                 <FaHeart className="" />
+                              </div>
+                              <div className="flex items-center justify-end mr-3 mt-[16px]">
+                                <FaCodeCompare className=""/>
+                              </div>
+                               <div className="flex items-center justify-end mr-3 mt-[16px]">
+                                 <p className="text-[#262626] hover:text-[#767676] text-[15px] font-dm font-bold hover:font-normal pr-2 cursor-pointer">Add to Cart</p>
+                                 <FaShoppingCart />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                  </div>
+                  <div className=" !w-[290px] relative group">
+                        <div className="w-full ">
+                            <img src={ari7Img} alt="" className="w-full" />
+                        </div>
+                        <div className="flex justify-between mt-5 z-10">
+                            <p className="text-[#262626] text-[16px] font-dm font-bold">Basic Crew  Neck Tee</p>
+                            <p className="text-[#767676] text-[16px] font-normal font-dm">$44.00</p>
+                        </div>
+                        <div className="absolute top-2 left-2">
+                          <p className="text-[#FFFFFF]  text-[14px] font-dm font-bold py-[7px] px-[28px] bg-black hover:bg-[#2e2b2b] cursor-pointer">New</p>
+                        </div>
+                        <div className="absolute left-0 bottom-[42px]">
+                          <div className="w-[290px] h-[0px] group-hover:h-[130px] duration-300 ease-in-out bg-[#FFFFFF]">
+                            <div className="pt-[26px] opacity-0 group-hover:opacity-95">
+                              <div className="flex items-center justify-end mr-3">
+                                 <p className="text-[#262626] hover:text-[#767676] text-[15px] font-dm font-bold hover:font-normal pr-2 cursor-pointer">Add to Wish List</p>
+                                 <FaHeart className="" />
+                              </div>
+                              <div className="flex items-center justify-end mr-3 mt-[16px]">
+                                <FaCodeCompare className=""/>
+                              </div>
+                               <div className="flex items-center justify-end mr-3 mt-[16px]">
+                                 <p className="text-[#262626] hover:text-[#767676] text-[15px] font-dm font-bold hover:font-normal pr-2 cursor-pointer">Add to Cart</p>
+                                 <FaShoppingCart />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                  </div>
+                  <div className=" !w-[290px] relative group">
+                        <div className="w-full ">
+                            <img src={ari8Img} alt="" className="w-full" />
+                        </div>
+                        <div className="flex justify-between mt-5 z-10">
+                            <p className="text-[#262626] text-[16px] font-dm font-bold">Basic Crew  Neck Tee</p>
+                            <p className="text-[#767676] text-[16px] font-normal font-dm">$44.00</p>
+                        </div>
+                        <div className="absolute top-2 left-2">
+                          <p className="text-[#FFFFFF]  text-[14px] font-dm font-bold py-[7px] px-[28px] bg-black hover:bg-[#2e2b2b] cursor-pointer">New</p>
+                        </div>
+                        <div className="absolute left-0 bottom-[42px]">
+                          <div className="w-[290px] h-[0px] group-hover:h-[130px] duration-300 ease-in-out bg-[#FFFFFF]">
+                            <div className="pt-[26px] opacity-0 group-hover:opacity-95">
+                              <div className="flex items-center justify-end mr-3">
+                                 <p className="text-[#262626] hover:text-[#767676] text-[15px] font-dm font-bold hover:font-normal pr-2 cursor-pointer">Add to Wish List</p>
+                                 <FaHeart className="" />
+                              </div>
+                              <div className="flex items-center justify-end mr-3 mt-[16px]">
+                                <FaCodeCompare className=""/>
+                              </div>
+                               <div className="flex items-center justify-end mr-3 mt-[16px]">
+                                 <p className="text-[#262626] hover:text-[#767676] text-[15px] font-dm font-bold hover:font-normal pr-2 cursor-pointer">Add to Cart</p>
+                                 <FaShoppingCart />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                  </div>
+                  <div className=" !w-[290px] relative group">
+                        <div className="w-full ">
+                            <img src={ari5Img} alt="" className="w-full" />
+                        </div>
+                        <div className="flex justify-between mt-5 z-10">
+                            <p className="text-[#262626] text-[16px] font-dm font-bold">Basic Crew  Neck Tee</p>
+                            <p className="text-[#767676] text-[16px] font-normal font-dm">$44.00</p>
+                        </div>
+                        <div className="absolute top-2 left-2">
+                          <p className="text-[#FFFFFF]  text-[14px] font-dm font-bold py-[7px] px-[28px] bg-black hover:bg-[#2e2b2b] cursor-pointer">New</p>
+                        </div>
+                        <div className="absolute left-0 bottom-[42px]">
+                          <div className="w-[290px] h-[0px] group-hover:h-[130px] duration-300 ease-in-out bg-[#FFFFFF]">
+                            <div className="pt-[26px] opacity-0 group-hover:opacity-95">
+                              <div className="flex items-center justify-end mr-3">
+                                 <p className="text-[#262626] hover:text-[#767676] text-[15px] font-dm font-bold hover:font-normal pr-2 cursor-pointer">Add to Wish List</p>
+                                 <FaHeart className="" />
+                              </div>
+                              <div className="flex items-center justify-end mr-3 mt-[16px]">
+                                <FaCodeCompare className=""/>
+                              </div>
+                               <div className="flex items-center justify-end mr-3 mt-[16px]">
+                                 <p className="text-[#262626] hover:text-[#767676] text-[15px] font-dm font-bold hover:font-normal pr-2 cursor-pointer">Add to Cart</p>
+                                 <FaShoppingCart />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                  </div>
+                  <div className=" !w-[290px] relative group">
+                        <div className="w-full ">
+                            <img src={ari6Img} alt="" className="w-full" />
+                        </div>
+                        <div className="flex justify-between mt-5 z-10">
+                            <p className="text-[#262626] text-[16px] font-dm font-bold">Basic Crew  Neck Tee</p>
+                            <p className="text-[#767676] text-[16px] font-normal font-dm">$44.00</p>
+                        </div>
+                        <div className="absolute top-2 left-2">
+                          <p className="text-[#FFFFFF]  text-[14px] font-dm font-bold py-[7px] px-[28px] bg-black hover:bg-[#2e2b2b] cursor-pointer">New</p>
+                        </div>
+                        <div className="absolute left-0 bottom-[42px]">
+                          <div className="w-[290px] h-[0px] group-hover:h-[130px] duration-300 ease-in-out bg-[#FFFFFF]">
+                            <div className="pt-[26px] opacity-0 group-hover:opacity-95">
+                              <div className="flex items-center justify-end mr-3">
+                                 <p className="text-[#262626] hover:text-[#767676] text-[15px] font-dm font-bold hover:font-normal pr-2 cursor-pointer">Add to Wish List</p>
+                                 <FaHeart className="" />
+                              </div>
+                              <div className="flex items-center justify-end mr-3 mt-[16px]">
+                                <FaCodeCompare className=""/>
+                              </div>
+                               <div className="flex items-center justify-end mr-3 mt-[16px]">
+                                 <p className="text-[#262626] hover:text-[#767676] text-[15px] font-dm font-bold hover:font-normal pr-2 cursor-pointer">Add to Cart</p>
+                                 <FaShoppingCart />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                  </div>
+
+            </Slider>
+            <div className="w-full">
+              <div className="lg:mt-[100px]">
+                <img src={addd2} alt="" className="w-full"/>
+              </div>
+            </div>
+        </div>
        </div>
      </div>
     </>
