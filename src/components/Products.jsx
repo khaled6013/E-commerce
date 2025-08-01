d86d44329f05572f22d6341a3e343999cc6ba278
khaled6013 <kmd601384@gmail.com>
2025-08-01 19:42:27 +0600

    product coler

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,3 +1,4 @@
+import { FaCaretDown } from "react-icons/fa"
 import { FiPlus } from "react-icons/fi"
 
 
@@ -37,6 +38,34 @@ const Products = () => {
                     <p className="text-[#767676] text-[16px] font-dm font-normal">Category 5</p>
                     <FiPlus />
                  </div>
+                </div>
+                <div className="mt-10 ">
+                    <div className="flex justify-between items-center">
+                        <h2 className="text-[#262626] text-[20px] font-dm font-bold">Shop by Color</h2>
+                    <FaCaretDown />
+                    </div>
+                    <div className="mt-8">
+                        <div className="border-b-[1px] border-[#F0F0F0] pb-3 flex items-center">
+                            <div className="bg-[#000000] h-[11px] w-[11px] rounded-full"></div>
+                           <p className="text-[#767676] text-[16px] font-dm font-normal pl-5">Color 1</p>
+                        </div>
+                        <div className="border-b-[1px] border-[#F0F0F0] pb-3 flex items-center pt-3">
+                            <div className="bg-[#FF8686] h-[11px] w-[11px] rounded-full"></div>
+                           <p className="text-[#767676] text-[16px] font-dm font-normal pl-5">Color 2</p>
+                        </div>
+                        <div className="border-b-[1px] border-[#F0F0F0] pb-3 flex items-center pt-3">
+                            <div className="bg-[#7ED321] h-[11px] w-[11px] rounded-full"></div>
+                           <p className="text-[#767676] text-[16px] font-dm font-normal pl-5">Color 3</p>
+                        </div>
+                        <div className="border-b-[1px] border-[#F0F0F0] pb-3 flex items-center  pt-3">
+                            <div className="bg-[#B6B6B6] h-[11px] w-[11px] rounded-full"></div>
+                           <p className="text-[#767676] text-[16px] font-dm font-normal pl-5">Color 4</p>
+                        </div>
+                        <div className="border-b-[1px] border-[#F0F0F0] pb-3 flex items-center pt-3">
+                            <div className="bg-[#15CBA5] h-[11px] w-[11px] rounded-full"></div>
+                           <p className="text-[#767676] text-[16px] font-dm font-normal pl-5">Color 5</p>
+                        </div>
+                    </div>
                 </div>
               </div>
             </div>
