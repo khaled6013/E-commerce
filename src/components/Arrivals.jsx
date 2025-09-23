d4fe962b075e5d40f8e5cc9efd9708b25458ce29
khaled6013 <kmd601384@gmail.com>
2025-09-23 20:57:37 +0600

    arrivals slide one

--- src/components/Arrivals.jsx
+++ src/components/Arrivals.jsx
@@ -118,45 +118,6 @@ const Arrivals = () => {
        </div>
        <div className="lg:w-10/12 mx-auto w-11/12">
         <div className="lg:mt-[150px]">
-           <Slider {...settings} className="gap-3" >
-                  {data.map((item)=>(
-                  <div className=" !w-[90%] relative group">
-                        <div className="w-full ">
-                          <Link to='/shop'>
-                            <img src={item.thumbnail} alt="" className="w-full" />
-                          </Link>
-                        </div>
-                        <div className="flex justify-between mt-5 z-10">
-                            <p className="text-[#262626] text-[16px] font-dm font-bold">{item.title}</p>
-                            <p className="text-[#767676] text-[16px] font-normal font-dm">${item.price}</p>
-                        </div>
-                        <div className="">
-                            <p className="text-[#767676] text-[16px] font-dm font-normal pt-3">Black</p>
-                        </div>
-                        <div className="absolute top-2 left-2">
-                          <p className="text-[#FFFFFF]  text-[14px] font-dm font-bold py-[7px] px-[28px] bg-black hover:bg-[#2e2b2b] cursor-pointer">{item.discountPercentage}</p>
-                        </div>
-                        <div className="absolute w-[100%] left-0 bottom-[62px]">
-                          <div className=" h-[0px] group-hover:h-[130px] duration-300 ease-in-out bg-[#FFFFFF]">
-                            <div className="pt-[26px] opacity-0 group-hover:opacity-95">
-                              <div className="flex items-center justify-end mr-3">
-                                 <p className="text-[#262626] hover:text-[#767676] text-[15px] font-dm font-bold hover:font-normal pr-2 cursor-pointer">Add to Wish List</p>
-                                 <FaHeart className="" />
-                              </div>
-                              <div className="flex items-center justify-end mr-3 mt-[16px]">
-                                <FaCodeCompare className=""/>
-                              </div>
-                               <div className="flex items-center justify-end mr-3 mt-[16px]">
-                                 <p className="text-[#262626] hover:text-[#767676] text-[15px] font-dm font-bold hover:font-normal pr-2 cursor-pointer">Add to Cart</p>
-                                 <FaShoppingCart />
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                  </div>
-                  ))}
-                 
-            </Slider>
             <div className="w-full">
               <div className="lg:mt-[100px]">
                 <img src={addd2} alt="" className="w-full"/>
