cf8528184279de170847c8dcfa1b54d7778291c1
khaled6013 <kmd601384@gmail.com>
2025-10-30 18:28:15 +0600

    return and add section full responsive done

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -7,22 +7,29 @@ const Information = () => {
     <>
       <div className="lg:py-7 py-3 border-b-2 border-[#F0F0F0]">
         <div className="lg:w-10/12 mx-auto w-11/12">
-          <div className="flex items-center">
-            <div className="lg:w-2/8 w-[30%]">
-               <div className="">
-                <span className="text-[#262626] lg:text-[20px] text-[15px] font-bold">2</span><span className="text-[#6D6D6D] lg:text-[16px] text-[14px] font-dm font-[400] lg:pl-3 pl-1">Two years warranty</span>
-               </div>
-            </div>
-            <div className="lg:w-4/8 w-[32%]">
-              <div className="flex items-center justify-center">
-                <MdLocalShipping/>
-                <p className="text-[#6D6D6D] lg:text-[16px] text-[14px] font-dm font-[400] lg:pl-3 pl-1">Free shipping</p>
+          <div className="flex flex-wrap items-center justify-between text-center lg:text-left">
+            <div className="lg:w-2/8 md:w-1/3 w-full mb-2 lg:mb-0 flex justify-center lg:justify-start">
+              <div className="flex items-center justify-center lg:justify-start">
+                <span className="text-[#262626] lg:text-[20px] text-[16px] font-bold">2</span>
+                <span className="text-[#6D6D6D] lg:text-[16px] text-[14px] font-dm font-[400] lg:pl-3 pl-1">
+                  Two years warranty
+                </span>
               </div>
             </div>
-            <div className="lg:w-2/8 w-[38%]">
-              <div className="flex items-center justify-end">
-                <TbReload/>
-                <p className="text-[#6D6D6D] lg:text-[16px] text-[14px]  font-dm font-[400] lg:pl-3 pl-1">Return policy in 30 days</p>
+            <div className="lg:w-4/8 md:w-1/3 w-full mb-2 lg:mb-0">
+              <div className="flex items-center justify-center">
+                <MdLocalShipping className="lg:text-[22px] text-[18px]" />
+                <p className="text-[#6D6D6D] lg:text-[16px] text-[14px] font-dm font-[400] lg:pl-3 pl-1">
+                  Free shipping
+                </p>
+              </div>
+            </div>
+            <div className="lg:w-2/8 md:w-1/3 w-full mb-2 lg:mb-0">
+              <div className="flex items-center justify-center lg:justify-end">
+                <TbReload className="lg:text-[22px] text-[18px]" />
+                <p className="text-[#6D6D6D] lg:text-[16px] text-[14px] font-dm font-[400] lg:pl-3 pl-1">
+                  Return policy in 30 days
+                </p>
               </div>
             </div>
           </div>
