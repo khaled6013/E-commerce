a65c2af6a13122508e5248918182b70f84c1360f
khaled6013 <kmd601384@gmail.com>
2025-12-25 22:49:08 +0600

    pagination update

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,24 +1,28 @@
-
-
-const Pagination = ({pageNumber , paginate,next,prev,currentPage,filterCate}) => {
-
+const Pagination = ({ pageNumber, paginate, next, prev, currentPage, filterCate }) => {
+    const handlePageChange = (index) => {
+        paginate(index);
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    };
 
     return (
         <>
-            <nav aria-label="Page navigation example" className={`${filterCate.length > 0 ? "hidden": ""}`}>
-                <ul class="inline-flex -space-x-px text-sm">
+            <nav aria-label="Page navigation example" className={`${filterCate.length > 0 ? "hidden" : ""}`}>
+                <ul className="inline-flex -space-x-px text-sm">
                     <li onClick={prev}>
-                        <a class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white cursor-pointer">Previous</a>
+                        <a className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 cursor-pointer">Previous</a>
                     </li>
-                    {pageNumber.map((item , i)=>(
-                    <li onClick={()=> paginate(i)}>
-                        <a className={`${currentPage == i+1?"flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-black border border-gray-300   dark:bg-[#FFFF] dark:border-gray-700 dark:text-black dark:hover:bg-gray-700  cursor-pointer" :"flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white cursor-pointer" }`}>
-                            {item}
-                        </a>
-                    </li>
+                    {pageNumber.map((item, i) => (
+                        <li key={i} onClick={() => handlePageChange(i)}>
+                            <a className={`${currentPage == i + 1 ? "flex items-center justify-center px-3 h-8 leading-tight text-white bg-black border border-gray-300 cursor-pointer" : "flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 cursor-pointer"}`}>
+                                {item}
+                            </a>
+                        </li>
                     ))}
                     <li onClick={next}>
-                        <a  class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white cursor-pointer">Next</a>
+                        <a className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 cursor-pointer">Next</a>
                     </li>
                 </ul>
             </nav>
@@ -26,4 +30,4 @@ const Pagination = ({pageNumber , paginate,next,prev,currentPage,filterCate}) =>
     )
 }
 
-export default Pagination
+export default Pagination;
